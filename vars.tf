96cb0d61690f6be2cf79132256d14b4b9b1515dc
Sanjana Soni <Sanjana.Soni2@ibm.com>
2022-07-26 11:56:17 +0530

    initial commit

---
+++ vars.tf
@@ -0,0 +1,32 @@
+# Configure these variables
+
+variable "sample_var" {
+  description = "A sample_var to pass to the template."
+  default     = "hello"
+}
+
+variable "sleepy_time" {
+  description = "How long our local-exec will take a nap."
+  default     = 0
+}
+
+variable "sleepy_time23" {
+  description = "How long our local-exec will take a nap."
+  default     = 1
+}
+
+variable "slash_branch-sample1" {
+  description = "How long our local-exec will take a nap."
+  default     = 3
+}
+
+variable "slash_branch-sample1-test" {
+  description = "How long our local-exec will take a nap."
+  default     = 4
+}
+
+variable "slash_branch-sample1-test2" {
+  description = "How long our local-exec will take a nap."
+  default     = 5
+}
+
